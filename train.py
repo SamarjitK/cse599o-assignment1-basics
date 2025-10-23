5fbcdf71826dd17b1b22f40ff41bc27a44b86c1f
Samarjit Kaushik <samarjit@tempura.cs.washington.edu>
2025-10-23 10:15:37 -0700

    editing code to report metrics

--- train.py
+++ train.py
@@ -1,25 +1,14 @@
-# Now, it’s finally time to put all of the components you implemented together into your main training
-#  script. It will pay off to make it easy to start training runs with different hyperparameters (e.g., by taking
-#  them as command-line arguments), since you will be doing these many times later to study how different
-#  choices impact training.
-# Deliverable: Write a script that runs a training loop to train your model on user-provided input. In
-#  particular, we recommend that your training script allow for (at least) the following:
-#  • Ability to configure and control the various model and optimizer hyperparameters.
-#  • Memory-efficient loading of large training and validation datasets with np.memmap.
-#  • Serializing checkpoints to a user-provided path.
-#  • Periodically logging training and validation performance (e.g., to console and/or an external
-#  service like Weights & Biases)a.
-
 import argparse
 import os
 import numpy as np
 import torch
+import time
 
 from cse599o_basics.transformer_lm import TransformerLM
 from cse599o_basics.tokenizer import BPETokenizer
 from cse599o_basics.adamw import AdamW
 from cse599o_basics.train_utils import get_batch, save_checkpoint, load_checkpoint
-from cse599o_basics.model_utils import cross_entropy, lr_scheduler, gradient_clipping
+from cse599o_basics.model_utils import cross_entropy, softmax, lr_scheduler, gradient_clipping
 
 def main():
     train_txt, valid_txt = "data/TinyStoriesV2-GPT4-train.txt", "data/TinyStoriesV2-GPT4-valid.txt"
@@ -39,8 +28,6 @@ def main():
     # training parameters
     parser.add_argument("--num_steps", type=int, default=5000)
 
-    # steps:
-    # first, parse args
     args = parser.parse_args()
     context_length: int = args.context_length
     num_layers: int = args.num_layers
@@ -51,36 +38,17 @@ def main():
     batch_size: int = args.batch_size
     num_steps: int = args.num_steps
 
-    # we have txt files, and a tokenizer. We need to read the txt files and tokenize them.
-    # then we can create a memmap dataset from the tokenized data, and use that for get_batch.
-    tokenizer = BPETokenizer(vocab={}, merges=[]) # should just load tiktoken.get_encoding("gpt2")
-    # get vocab size from tokenizer, add to args
+    print("Initializing tokenizer...")
+    tokenizer = BPETokenizer(vocab={}, merges=[])
     vocab_size = tokenizer.tokenizer.n_vocab
 
-    # create memmap datasets
-    if not os.path.exists("data/train_memmap.dat"):
-        print("Tokenizing training data...")
-        with open(train_txt, "r", encoding="utf-8") as f:
-            train_data = f.read()
-        train_tokens = tokenizer.encode(train_data)
-        print("Creating training memmap dataset...")
-        train_dataset = np.memmap("data/train_memmap.dat", dtype=np.uint16, mode="w+", shape=(len(train_tokens),))
-        train_dataset[:] = np.array(train_tokens, dtype=np.uint16)
-    if not os.path.exists("data/valid_memmap.dat"):
-        print("Repeating for validation data...")
-        with open(valid_txt, "r", encoding="utf-8") as f:
-            valid_data = f.read()
-        valid_tokens = tokenizer.encode(valid_data)
-        valid_dataset = np.memmap("data/valid_memmap.dat", dtype=np.uint16, mode="w+", shape=(len(valid_tokens),))
-        valid_dataset[:] = np.array(valid_tokens, dtype=np.uint16)
+    print("Preparing datasets...")
+    train_dataset, valid_dataset = prep_datasets(train_txt, valid_txt, tokenizer)
 
-    train_dataset = np.memmap("data/train_memmap.dat", dtype=np.uint16, mode="r")
-    valid_dataset = np.memmap("data/valid_memmap.dat", dtype=np.uint16, mode="r")
+    print("Initializing model and optimizer...")
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Using device: {device}")
-
-    print("Initializing model and optimizer...")
 
     model = TransformerLM(
         vocab_size=vocab_size,
@@ -99,7 +67,37 @@ def main():
     )
 
     print("Starting training loop...")
+    train(model, optim, train_dataset, valid_dataset, batch_size, context_length, num_steps, device)
 
+    decode(model, tokenizer, optim, max_tokens=30, temperature=1.0, top_p=0.9, prompt="Once upon a time", ckpt_file="")
+
+def prep_datasets(train_txt: str, valid_txt: str, tokenizer: BPETokenizer):
+    if not os.path.exists("data/train_memmap.dat"):
+        print("Tokenizing training data...")
+        with open(train_txt, "r", encoding="utf-8") as f:
+            train_data = f.read()
+        train_tokens = tokenizer.encode(train_data)
+        print("Creating training memmap dataset...")
+        train_dataset = np.memmap("data/train_memmap.dat", dtype=np.uint16, mode="w+", shape=(len(train_tokens),))
+        train_dataset[:] = np.array(train_tokens, dtype=np.uint16)
+    if not os.path.exists("data/valid_memmap.dat"):
+        print("Repeating for validation data...")
+        with open(valid_txt, "r", encoding="utf-8") as f:
+            valid_data = f.read()
+        valid_tokens = tokenizer.encode(valid_data)
+        valid_dataset = np.memmap("data/valid_memmap.dat", dtype=np.uint16, mode="w+", shape=(len(valid_tokens),))
+        valid_dataset[:] = np.array(valid_tokens, dtype=np.uint16)
+
+    train_dataset = np.memmap("data/train_memmap.dat", dtype=np.uint16, mode="r")
+    valid_dataset = np.memmap("data/valid_memmap.dat", dtype=np.uint16, mode="r")
+    return train_dataset, valid_dataset
+
+def train(model: TransformerLM, optim: AdamW, 
+          train_dataset: np.memmap, valid_dataset: np.memmap,
+          batch_size, context_length, num_steps, device):
+    # checkpoint file name: current timestamp (in progress work here)
+    file_name = str(int(time.time()))
+    # inputs, labels = get_batch(train_dataset, batch_size, context_length, device)
     for step in range(1, num_steps + 1):
         model.train()
         inputs, labels = get_batch(train_dataset, batch_size, context_length, device)
@@ -107,31 +105,67 @@ def main():
         outputs = model(inputs)
         loss = cross_entropy(outputs, labels)
         loss.backward()
-        # gradient_clipping(model, max_norm=1.0)
-        # lr = lr_scheduler(step, args.num_steps, initial_lr=1e-4, final_lr=1e-5)
-        # for param_group in optim.param_groups:
-        #     param_group['lr'] = lr
         optim.step()
 
-        if num_steps < 50 or step % (num_steps // 50) == 0:
+        if (num_steps < 50 and step % 2 == 0)  or (num_steps >= 50 and step % (num_steps // 50) == 0):
             print(f"Step {step}, Training Loss: {loss.item():.4f}")
 
-        if (num_steps < 10 and step % 2 == 0) or (num_steps > 10 and step % (num_steps // 10)) == 0:
+        if (num_steps < 20 and step % 5 == 0) or (num_steps >= 20 and step % (num_steps // 10) == 0):
             model.eval()
             with torch.no_grad():
-                val_inputs, val_labels = get_batch(valid_dataset, args.batch_size, args.context_length, device)
+                val_inputs, val_labels = get_batch(valid_dataset, batch_size, context_length, device)
                 val_outputs = model(val_inputs)
                 val_loss = cross_entropy(val_outputs, val_labels)
                 print(f"Step {step}, Validation Loss: {val_loss.item():.4f}")
 
-            # use directory, actually
             save_checkpoint(model, optim, step, os.path.join("checkpoints", f"checkpoint_step_{step}.pt"))
+        
 
-        # How to run:
-# python train.py --num_steps 5000
-# on slurm:
-# 
+def decode(model: TransformerLM, tokenizer: BPETokenizer, optim: AdamW,
+           max_tokens:int = 256, temperature: float = 1.0, top_p: float = 0.9, 
+           prompt: str = "Once upon a time", ckpt_file: str = ""):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Using device: {device}")
 
+    if ckpt_file and os.path.exists(ckpt_file):
+        print("Loading checkpoint...")
+        load_checkpoint(ckpt_file, model, optim)
+    else:
+        print("No checkpoint. I sure hope you've trained this model!")
+
+    print("Generating text...")
+    model.eval()
+    input_tokens = torch.tensor(tokenizer.encode(prompt)) # (seq_len,)
+    input_tensor = input_tokens.unsqueeze(0).to(device) # (1, seq_len)
+    eot = tokenizer.tokenizer.eot_token
+
+    # keep list of generated tokens
+    generated_tokens = input_tokens.tolist()
+
+    with torch.no_grad():
+        for _ in range(max_tokens):
+            outputs = model(input_tensor) # (1, seq_len, vocab_size)
+            last_token_softmax = softmax(outputs[0, -1, :] / temperature, dim=-1) # (vocab_size,)
+            sorted_probs, sorted_indices = torch.sort(last_token_softmax, descending=True)
+            sum = 0.0
+            i = 0
+            while sum < top_p and i < sorted_probs.size(0):
+                sum += sorted_probs[i].item()
+                i += 1
+            top_p_indices = sorted_indices[:i]
+            top_p_probs = sorted_probs[:i]
+            top_p_probs = top_p_probs / top_p_probs.sum()
+
+            index = torch.multinomial(top_p_probs, 1)
+            next_token = top_p_indices[index] # (1,)
+            generated_tokens.append(next_token.item())
+            if next_token.item() == eot:
+                break
+            input_tensor = torch.cat([input_tensor, next_token.unsqueeze(0).to(device)], dim=1)
+
+    generated_text = tokenizer.decode(generated_tokens)
+    print("Generated Text:")
+    print(generated_text)
 
 if __name__ == "__main__":
     main()
